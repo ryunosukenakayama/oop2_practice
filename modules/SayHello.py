e8c8d8078ffa341bce6065c3b2ddceaaa209959f
ryunosukenakayama <k19076kk@aitech.ac.jp>
2021-12-09 14:10:17 +0900

    change: targe -> target

--- modules/SayHello.py
+++ modules/SayHello.py
@@ -1,9 +1,10 @@
 class SayHello:
     def __init__(self, target="World"):
-         self.targe=target
+         self.target = target
 
     def say(self):
         print(f"Hello, {self.target}!!")
+
 if __name__ == '__main__':
     app = SayHello()
     app.say()
